9c2b36a56ef958b8b715b3799bcf11e7fbd4c730
Quinten van der Post <qvdpost@gmail.com>
2016-11-22 11:39:36 +0100

    Initial Commit

---
+++ 10272380-pset4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  10272380-pset4
+//
+//  Created by Quinten van der Post on 22/11/2016.
+//  Copyright © 2016 Quinten van der Post. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
